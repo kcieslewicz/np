20eedd0516e864e8fba7c537b9fa0170714e5afa
kcieslewicz <karcie003@utp.edu.pl>
2019-01-11 16:23:57 +0100

    main + funkcja

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include <conio.h>
+#include <head.h>
 
 using namespace std;
 
 int main(){
 
-_getch();
-return 0;
+	int a;
+	cout << "Podaj liczbe";
+	cin >> a;
+	boolean even=even(a);
+	if(even==true){
+		cout << "Zmienna parzysta";
+	}
+	else{
+		cout << "Zmienna nieparzysta";
+	}
+
+	_getch();
+	return 0;
 }
